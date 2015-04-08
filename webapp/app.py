4e9e1917692f79242bae6e6e3d2e5a3bac8f0b69
EwyynTomato <nkaloli@gmail.com>
2015-04-08 10:37:20 +0800

    added: logging

--- webapp/app.py
+++ webapp/app.py
@@ -1,9 +1,11 @@
+from utils.Logger import logging
 from flask import Flask, request
 from route import ApiRoutes
 from utils.AppConfig import config
 
 app = Flask(__name__)
 app.register_blueprint(ApiRoutes.apiroutes)
+app.logger.addHandler(logging.handlers)
 config.setup(app)
 
 if __name__ == '__main__':
--- webapp/route/ApiRoutes.py
+++ webapp/route/ApiRoutes.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, current_app, Response, request, render_template, redirect, escape
+import logging
+import traceback
+from flask import Blueprint, request, render_template, redirect, escape, session
 from texttemplatematcher import fuzzymatcher, difflibmatcher
-from utils.AppConfig import config
 
 apiroutes = Blueprint('apiroutes', __name__)
 
 class ApiRoutesConst(object):
-    INDEX        = '/'
-    HEALTH_CHECK = '/api/health_check'
+    INDEX          = '/'
+    HEALTH_CHECK   = '/api/health_check'
+    MATCH_TEMPLATE = '/match_template'
 
 @apiroutes.route(ApiRoutesConst.INDEX)
 def index():
@@ -17,7 +19,7 @@ def index():
 def health_check():
     return 'Api is alive'
 
-@apiroutes.route("/match_template", methods=["GET", "POST"])
+@apiroutes.route(ApiRoutesConst.MATCH_TEMPLATE, methods=["GET", "POST"])
 def match_template():
     if request.method == "GET":
         return redirect(ApiRoutesConst.INDEX)
@@ -26,10 +28,20 @@ def match_template():
     escapedtext     = str(escape(text))     #Prevent XSS
     escapedtemplate = str(escape(template)) #Prevent XSS
 
+    logging.debug("form: {:}".format(str(request.form)))
+
     match_functions = [
         {"function":"Fuzzy (Levenshtein)",
          "result": fuzzymatcher.mark(escapedtext, fuzzymatcher.template_match(escapedtext, escapedtemplate).vars, prefix='<mark>', suffix='</mark>')}
         , {"function":"Simple Sequence Matcher (Diff)",
            "result": difflibmatcher.mark(escapedtext, difflibmatcher.template_match(escapedtext, escapedtemplate), prefix='<mark>', suffix='</mark>')}
-                       ]
+    ]
+
+    logging.debug("result: {:}".format(match_functions))
+
     return render_template("index.html", previnput={"template":template, "inputtext":text}, match_functions=match_functions)
+
+@apiroutes.errorhandler(Exception)
+def errorhandler(uncaught_exception):
+    logging.error("Uncaught exception for request: " + str(request.form))
+    logging.error(uncaught_exception)
