138f9bf643113b3f098c717072f0bcce4d9d97a4
EwyynTomato <nkaloli@gmail.com>
2015-04-08 14:29:03 +0800

    added version

---
+++ texttemplatematcher/texttemplatematcher/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+PyTextTemplateMatcher
+~~~~~~~~~~~~~~~~~~~~~
+
+Simple template matcher for text.
+"""
+
+__title__ = 'texttemplatematcher'
+__version__ = '0.0.1'
+__author__ = 'ElwyynTomato'
